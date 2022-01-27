652d456b240e832e0949770c4e99a421a91115fe
akif171 <akifali229@gmail.com>
2022-01-28 00:47:49 +0500

    Markdown Blog with node express mongodb

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const Article = require("./models/article");
+const mongoose = require("mongoose");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+
+mongoose.connect("mongodb://localhost/blog");
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(3000);
